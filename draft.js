9dcf460fca5991c65b4f1ddd1ee4739358e3be7e
slowrunr <mungaslov.nft@gmail.com>
2023-08-27 22:31:16 +0900

    added new files to branch

---
+++ draft.js
@@ -0,0 +1,10 @@
+// функция сворачивания окна при нажатии за пределами меню
+// dropdownMenuNode.addEventListener("click", (event) => {
+//   const isClickOutsideContent = !event
+//     .composedPath()
+//     .includes(dropdownMenuNode);
+
+//   if (isClickOutsideContent) {
+//     dropdownMenuNode.classList.remove("active");
+//   }
+// });
