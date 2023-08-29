3eaae593dfc6433f7c5e1afbd7c6ab96b00c1509
slowrunr <mungaslov.nft@gmail.com>
2023-08-29 22:57:30 +0900

    added function addMovieButtonHandler()

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-    
+const STATUS_OUT_OF_DATA_CLASSNAME = "border-red";
+
+const movieTitleInputNode = document.getElementById("movieInput");
+const addMovieBtnNode = document.getElementById("addMovieBtn");
+const movieListNode = document.getElementById("movieList");
+
+let watchList = [];
+
+const addMovieHandler = () => {
+  if (!movieTitleInputNode.value) {
+    // movieTitleInputNode.classlist.add("border-red");
+    return;
+  }
+  //   movieTitleInputNode.classlist.remove(STATUS_OUT_OF_DATA_CLASSNAME);
+  const watchListItem = movieTitleInputNode.value;
+  watchList.push(watchListItem);
+  console.log(watchList);
+  movieTitleInputNode.value = "";
+};
+
+addMovieBtnNode.addEventListener("click", addMovieHandler);
