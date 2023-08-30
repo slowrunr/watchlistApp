4a220a3fd3e578bca293f048aefd86a87ca83a7e
slowrunr <mungaslov.nft@gmail.com>
2023-08-30 22:28:24 +0900

    added movielist output

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const STATUS_OUT_OF_DATA_CLASSNAME = "border-red";
+ITEMS_FROM_STORAGE = "stored-items";
 
 const movieTitleInputNode = document.getElementById("movieInput");
 const addMovieBtnNode = document.getElementById("addMovieBtn");
@@ -13,9 +14,34 @@ const addMovieHandler = () => {
   }
   //   movieTitleInputNode.classlist.remove(STATUS_OUT_OF_DATA_CLASSNAME);
   const watchListItem = movieTitleInputNode.value;
-  watchList.push(watchListItem);
-  console.log(watchList);
   movieTitleInputNode.value = "";
+  watchList.push(watchListItem);
+  // saveItemsInLocalStorage();
+  console.log(watchList);
+
+  let watchListHTML = "";
+  watchList.forEach((element) => {
+    watchListHTML += `<li id="movieTitleWrapper" class="movie-title-wrapper">
+      <div class="checkbox-wrapper">
+      <button class="checkbox"></button>
+      </div>
+      <p id="movieTitle" class="movie-title">${element}</p>
+      <div class="close-btn-wrapper">
+      <img
+      id="removeFromListBtn"
+      class="remove-from-list-btn"
+      src="icons/close-block-icon.png"
+      />
+      </div>
+    </li>`;
+  });
+  movieListNode.innerHTML = `<ol>${watchListHTML}</ol>`;
 };
+
+// function saveItemsInLocalStorage() {
+//   const storedMoviesTitles = watchListItem;
+//   localStorage.setItem(ITEMS_FROM_STORAGE, storedMoviesTitles);
+//   console.log(expensesToString);
+// }
 
 addMovieBtnNode.addEventListener("click", addMovieHandler);
