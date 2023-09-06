1269cb0bd8186993d6067d9028a101d5d6096207
slowrunr <mungaslov.nft@gmail.com>
2023-09-07 02:20:16 +0900

    workin on actions with Items as objects

--- index.js
+++ index.js
@@ -1,94 +1,111 @@
 const TITLES_FROM_STORAGE = "saved-items";
-const MARK_AS_WATCHED = "checked";
+const CROSSED_OUT_TITLE = "crossed-out";
+const ITEM_STATUS_WATCHED = "watched";
+const SHADED_ITEM_WRAPPER = "shaded";
 const STATUS_OUT_OF_DATA_CLASSNAME = "border-red";
 
 const titleInputNode = document.getElementById("titleInput");
 const addMovieBtnNode = document.getElementById("addToWatchBtn");
 const watchlistNode = document.getElementById("watchlist");
-// const checkboxNode = document.querySelector("checkbox");
-// const watchlistItemTitle = document.getElementById("watchlist-item-title");
 
-// создаем массив для списка для просмотра (далее - список)
 let watchlist = [];
 
-const getItemsFromLocalStorage = () => {
-  if (localStorage.getItem(TITLES_FROM_STORAGE)) {
-    movies = JSON.parse(localStorage.getItem(TITLES_FROM_STORAGE));
-  }
-  renderWatchlist();
-};
-
-const saveItemsInLocalStorage = () => {
-  localStorage.setItem(TITLES_FROM_STORAGE, JSON.stringify(watchlist));
-};
-
-//+ функция вывода списка на странице
-const renderWatchlist = () => {
-  watchlist.forEach((watchlistItem) => {
-    const watchlistHTML = `<li id="watchlistItemWrapper" class="watchlist-item-wrapper" >
-      <div class="checkbox-wrapper">
-        <button class='status-checkbox' data-action="done"></button>
-      </div>
-      <span class='watchlist-item-title watchlist-item-title--done'>${watchlistItem.title}</span>
-      <div class="close-btn-wrapper">
-        <button id="removeFromListBtn"
-        class="remove-from-list-btn"
-        data-action="delete"></button>
-       </div>
-    </li>`;
-
-    watchlistNode.insertAdjacentHTML("beforeend", watchlistHTML);
-  });
-};
-
-getItemsFromLocalStorage();
-
-//+ функция-обработчик для кнопки (add-2watch-btn) добавления названия контента в список просмотра
-const addToWatchBtnHandler = () => {
+function addToWatchBtnHandler() {
   if (!titleInputNode.value) {
     titleInputNode.classList.add(STATUS_OUT_OF_DATA_CLASSNAME);
     return;
   }
   titleInputNode.classList.remove(STATUS_OUT_OF_DATA_CLASSNAME);
+
   const watchlistItemTitle = titleInputNode.value.trim();
-  titleInputNode.value = "";
-  titleInputNode.focus();
+
   const watchlistItem = {
-    title: watchlistItemTitle,
-    checked: false,
+    id: Date.now(),
+    text: watchlistItemTitle,
+    itemWrapperStyle: false,
+    checkboxStyle: false,
+    watchlistTitleStyle: false,
   };
+
+  const itemWrapperStyle = watchlistItem.itemWrapperStyle
+    ? "watchlist-item-wrapper shaded"
+    : "watchlist-item-wrapper";
+
+  const checkboxStyle = watchlistItem.checkboxStyleStyle
+    ? "status-checkbox watched"
+    : "status-checkbox";
+
+  const watchlistTitleStyle = watchlistItem.watchlistTitleStyle
+    ? "watchlist-item-title crossed-out"
+    : "watchlist-item-title";
+
+  const watchlistHTML = `<li id="${watchlistItem.id}" class="${itemWrapperStyle}" >
+    <div class="checkbox-wrapper">
+      <button class='${checkboxStyle}' data-action="done"></button>
+    </div>
+    <span class='${watchlistTitleStyle}'>${watchlistItem.text}</span>
+    <div class="close-btn-wrapper">
+      <button id="removeFromListBtn"
+      class="remove-from-list-btn"
+      data-action="delete"></button>
+     </div>
+  </li>`;
+
+  watchlistNode.insertAdjacentHTML("beforeend", watchlistHTML);
+
   watchlist.push(watchlistItem);
-  saveItemsInLocalStorage();
+
   console.log(watchlist);
-  renderWatchlist();
-};
 
-const markIfWatchedHandler = () => {
-  checkbox.style.background = "#6532f8";
-};
+  saveItemsInLocalStorage();
 
-//
+  titleInputNode.value = "";
+
+  titleInputNode.focus();
+}
+
+// function renderWatchlistItem(watchlistItem) {
+
+// }
+
 function removeItemFromList(e) {
-  if (e.target.dataset.action === "delete") {
-    const parentNode = e.target.closest("li");
-    parentNode.remove();
-  }
+  if (e.target.dataset.action !== "delete") return;
+  const parentNode = e.target.closest("li");
+  const watchlistItemId = Number(parentNode.id);
+  const index = watchlist.findIndex(
+    (watchlistItem) => watchlistItem.id === watchlistItemId
+  );
+
+  // - deleting Items via array filtering
+  // watchlist = watchlist.filter(
+  //   (watchlistItem) => watchlistItem.id !== watchlistItemId
+  // );
+  console.log(index);
+  watchlist.splice(index, 1);
+  parentNode.remove();
 }
 
 function markItemAsWatched(e) {
-  if (e.target.dataset.action === "done") {
-    const parentNode = e.target.closest("li");
-    const itemStatus = parentNode.querySelector(".status-checkbox");
-    const watchlistItemTitle = parentNode.querySelector(
-      ".watchlist-item-title"
-    );
-    parentNode.classList.add("shaded");
-    itemStatus.classList.add("watched");
-    watchlistItemTitle.classList.add("crossed-out");
-  }
+  if (e.target.dataset.action !== "done") return;
+  const parentNode = e.target.closest("li");
+  const itemStatus = parentNode.querySelector(".status-checkbox");
+  const watchlistItemTitle = parentNode.querySelector(".watchlist-item-title");
+  parentNode.classList.toggle(SHADED_ITEM_WRAPPER);
+  itemStatus.classList.toggle(ITEM_STATUS_WATCHED);
+  watchlistItemTitle.classList.toggle(CROSSED_OUT_TITLE);
 }
 
 addMovieBtnNode.addEventListener("click", addToWatchBtnHandler);
 watchlistNode.addEventListener("click", removeItemFromList);
 watchlistNode.addEventListener("click", markItemAsWatched);
-// movieListNode.addEventListener('click', removeFromListHandler);
+
+function getItemsFromLocalStorage() {
+  if (localStorage.getItem(TITLES_FROM_STORAGE)) {
+    movies = JSON.parse(localStorage.getItem(TITLES_FROM_STORAGE));
+  }
+  renderWatchlistItem();
+}
+
+function saveItemsInLocalStorage() {
+  localStorage.setItem(TITLES_FROM_STORAGE, JSON.stringify(watchlist));
+}
